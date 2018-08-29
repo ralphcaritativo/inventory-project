624fcef7f5498cfcd69b5e79a1e901d17a7eaccc
Ralph Caritativo <ralph07caritativo@gmail.com>
2018-08-29 09:55:04 +0800

    Registration and Login DONE

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+# from django.contrib.auth.decorators  import login_required
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
+                auth.login(request, user)
+                return redirect('login')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Password must match'})
+    else:
+        return render(request, 'accounts/signup.html')
+
+# @login_required(login_url = "")
+def login(request):
+	if request.method == 'POST':
+		user = auth.authenticate(username = request.POST['username'], password = request.POST['password'])
+		if user is not None:
+			auth.login(request, user)
+			return render(request, 'dashboard.html', {'username': request.POST['username']})
+		else:
+			return render(request, 'accounts/login.html', {'error': 'Username or Password is incorrect! '})
+	else:
+		return render(request, 'accounts/login.html')
+
+# TODO need to add to Homepage
+
+def logout(request):
+	if request.method == 'POST':
+		auth.logout(request)
+		return redirect('login')
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products import views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.login, name = 'login'),
+    path('', include('accounts.urls')),
     path('dashboard/', views.dashboard, name = 'dashboard'),
+    path('accounts/', include('accounts.urls')),
 ]
